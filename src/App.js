0220e79a46d45b1bc6556952e90e6efa84a04986
rakibul58 <rhrahi14@gmail.com>
2022-10-16 19:05:33 +0600

    complete for now

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home';
+import Login from './components/Login';
+import Orders from './components/Orders';
+import Register from './components/Register';
+import Main from './layout/Main';
+import PrivateRoute from './routes/PrivateRoute';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element:<Main/>,
+      children: [
+        {
+          path: '/',
+          element: <Home/>
+        },
+        {
+          path: '/orders',
+          element: <PrivateRoute><Orders></Orders></PrivateRoute>
+        },
+        {
+          path: '/login',
+          element: <Login/>
+        },
+        {
+          path: '/register',
+          element: <Register/>
+        }
+      ]
+    }
+  ])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <RouterProvider router={router}/>
     </div>
   );
 }
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDpM2tkJcIhljcw2o1vTUfFgcwFPp7MSa8",
+  authDomain: "auth-router-context-59e8d.firebaseapp.com",
+  projectId: "auth-router-context-59e8d",
+  storageBucket: "auth-router-context-59e8d.appspot.com",
+  messagingSenderId: "594803627988",
+  appId: "1:594803627988:web:6a05358eb11d0658f62841"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
